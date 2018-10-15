a42eba6445b13f243dc358157acdffc7ad0ec2c1
Daniel Tang <danielzgtg.opensource@gmail.com>
2018-10-15 04:57:23 -0400

    Add proguard

--- src/main/java/io/github/e_vent/buildsystem/textprocessor/ProcessorLink.java
+++ src/main/java/io/github/e_vent/buildsystem/textprocessor/ProcessorLink.java
@@ -1,6 +1,7 @@
 package io.github.e_vent.buildsystem.textprocessor;
 
 import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ProcessorLink implements ITextProcessor {
@@ -15,12 +16,20 @@ public class ProcessorLink implements ITextProcessor {
 
 	@Override
 	public final String process(final String input, final String name) {
-		return REGEX.matcher(input).replaceAll(x -> doReplace(x, name));
+		// workaround for lack of Matcher.replaceAll(Function<MatchResult, String>) before Java 9
+		final Matcher matcher = REGEX.matcher(input);
+		matcher.reset();
+		final StringBuffer result = new StringBuffer();
+		while (matcher.find()) {
+			matcher.appendReplacement(result, doReplace(matcher, name));
+		}
+		matcher.appendTail(result);
+		return result.toString();
 	}
 
 	private final String doReplace(final MatchResult match, final String name) {
 		final String linkTarget = match.group(1);
-		String s = name.equals(linkTarget) ? "#" : linkTarget + ".html#";
+		final String s = name.equals(linkTarget) ? "#" : linkTarget + ".html#";
 		//System.err.println("DEBUG: " + s);
 		return s;
 	}
